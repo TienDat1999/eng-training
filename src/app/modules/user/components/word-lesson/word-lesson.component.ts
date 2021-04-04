ffb07ef71c29a2d44406ee4c082fd60f792701fd
TienDat1999 <letiendat1999py@gmail.com>
2021-04-04 15:32:46 +0700

    write css for topic

--- src/app/modules/user/components/word-lesson/word-lesson.component.ts
+++ src/app/modules/user/components/word-lesson/word-lesson.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {CourseCardService} from '@app/modules/user/services/course-card.service';
 import {WordModel} from '@app/modules/user/models/userModel';
+import {ActivatedRoute, Router} from '@angular/router';
+
 @Component({
   selector: 'app-word-lesson',
   templateUrl: './word-lesson.component.html',
@@ -9,13 +11,24 @@ import {WordModel} from '@app/modules/user/models/userModel';
 export class WordLessonComponent implements OnInit {
 
   words: WordModel[] = [];
-  constructor(private wordService: CourseCardService) { }
+  levelParam: number;
+  parentParam: string;
+  nextLevelParam: any;
+  istrue = false;
+  constructor(private wordService: CourseCardService, private route: ActivatedRoute,  private router: Router) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( paramMap => {
+      this.parentParam = paramMap.get('name');
+      this.levelParam = +paramMap.get('id');
+      this.nextLevelParam =  +paramMap.get('id') + 1;
+    });
     this.wordService.getWord().subscribe(value => {
-      console.log(value);
       this.words = value;
     });
   }
 
+  goToNextPage(): void {
+    this.router.navigate(['/learn', this.parentParam, this.nextLevelParam]);
+  }
 }
