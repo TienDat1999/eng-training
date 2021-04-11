76272fbd6854a9ee141753d7543340617218a999
Thanh Lam <hothanhlam197@gmail.com>
2021-04-11 15:27:44 +0700

    [CLASS] [ranking] implement preliminary ranking

--- src/app/modules/user/components/class/ranking/ranking.component.ts
+++ src/app/modules/user/components/class/ranking/ranking.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RankingComponent implements OnInit {
 
+  members = [
+    {id: 1, name: 'Mark Otto', score: 12},
+    {id: 2, name: 'Jacob Thornton', score: 15},
+    {id: 3, name: 'Thornton Bird', score: 32},
+    {id: 3, name: 'Thornton Bird', score: 32},
+    {id: 3, name: 'Thornton Bird', score: 32},
+    {id: 3, name: 'Thornton Bird', score: 32},
+    {id: 3, name: 'Thornton Bird', score: 32},
+    {id: 3, name: 'Thornton Bird', score: 32},
+    {id: 3, name: 'Thornton Bird', score: 32},
+    {id: 3, name: 'Thornton Bird', score: 32},
+    {id: 3, name: 'Thornton Bird', score: 32},
+    {id: 3, name: 'Thornton Bird', score: 32},
+    {id: 3, name: 'Thornton Bird', score: 32},
+    {id: 3, name: 'Thornton Bird', score: 32},
+  ];
   constructor() { }
 
   ngOnInit(): void {
