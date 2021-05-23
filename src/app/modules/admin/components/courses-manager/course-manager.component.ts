b6697702739785fd908b7d7c7693873f76dd4730
Thanh Lam <hothanhlam197@gmail.com>
2021-05-23 23:58:21 +0700

    course-manager

--- src/app/modules/admin/components/courses-manager/course-manager.component.ts
+++ src/app/modules/admin/components/courses-manager/course-manager.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-manager.component.scss']
 })
 export class CourseManagerComponent implements OnInit {
-
+  dataSource: [];
   constructor() { }
 
   ngOnInit(): void {
